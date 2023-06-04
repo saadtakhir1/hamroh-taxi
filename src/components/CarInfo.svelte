54cc957e38f9535ca095f1f109ea1348c34b8230
taxirov <saadtakhir1@gmail.com>
2023-06-05 03:20:56 +0500

    zz

--- src/components/CarInfo.svelte
+++ src/components/CarInfo.svelte
@@ -4,20 +4,8 @@
     export let showme: boolean
     let showEdit: boolean = false
 
-    type Car = {
-        id: number
-        car_model: string
-        car_number: string
-        user: number
-    }
+    let car = JSON.parse(localStorage.getItem('car'))
 
-    let car: Car
-
-    function getCarInfo() {
-        car = JSON.parse(localStorage.getItem('car'))
-    }
-
-    getCarInfo()
 
 </script>
 
@@ -39,4 +27,6 @@
         <button on:click={() => { showEdit = true}} class="bg-gradient-to-b from-indigo-500 from-1%  to-indigo-900 to-100% text-white text-xs md:text-sm font-semibold rounded-xl py-3 px-5"><i class="bi bi-pencil"></i> Avtomobilni tahrirlash</button>
     </div>
     <CarEdit show={showEdit} close={() => { showEdit = false}} car_model={car.car_model} car_number={car.car_number} id={car.id}></CarEdit>
+{:else}
+    <p>no car</p>
 {/if}
